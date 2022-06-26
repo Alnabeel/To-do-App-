fed8f4981a255b63c7a0d2c98139871d6ed1bd9d
alnabeel <alnabeel27@gmail.com>
2022-06-26 21:36:48 +0530

    edit button add

--- app.js
+++ app.js
@@ -7,32 +7,47 @@ function addlist(e) {
     const completedtask= document.querySelector(".completedtask");
 
         let newlist= document.createElement("li");
+        let span=document.createElement("span")
         let completebutton=document.createElement("button");
         let deletebutton=document.createElement("button");
+        let editbutton= document.createElement("button")
          let btnvalue1=document.createTextNode("Complete");
          let btnvalue2= document.createTextNode("Delete")
+         let btnvalue3= document.createTextNode("Edit")
          completebutton.appendChild(btnvalue1);
          deletebutton.appendChild(btnvalue2);
-            
+         editbutton.appendChild(btnvalue3);
+        
         if(input.value==""){
             alert("Please Enter Your Task");
         }else{
-            newlist.textContent=input.value;
-            input.value="";
+           
+         console.log(input.value)
 
             notcompleted.appendChild(newlist);
+            newlist.appendChild(span)
             newlist.appendChild(completebutton);
             newlist.appendChild(deletebutton);
+            newlist.appendChild(editbutton);
+           newlist.firstElementChild.textContent=input.value;
+            input.value="";          
         }
         completebutton.addEventListener("click", function(){
             const parent= this.parentNode;
             parent.remove(parent);
             completedtask.appendChild(parent);
-            completebutton.style.display="none"            
+            completebutton.style.display="none" ;
+            editbutton.style.display="none" ;          
         
         });
         deletebutton.addEventListener("click", function(){
             const parent= this.parentNode;
             parent.remove(parent);
-        })   
+        });
+        editbutton.addEventListener("click",function(){
+            const parentvalue=this.parentElement.firstChild.textContent;
+            input.value=parentvalue;
+            this.parentNode.remove();
+            console.log(parentvalue);
+        })
 }  
